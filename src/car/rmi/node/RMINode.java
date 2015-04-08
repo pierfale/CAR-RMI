ca7a527ab5f9843ccef665bfd5c23ba5c1eb30aa
GaetanDeflandre <gaetan.deflandre@gmail.com>
2015-04-08 14:52:49 +0200

    ajoute de la doc sur l'interface

--- src/car/rmi/node/RMINode.java
+++ src/car/rmi/node/RMINode.java
@@ -5,51 +5,63 @@ import java.rmi.*;
 import car.rmi.trace.Trace;
 
 /**
- * RMI interface for a node
- * A node own an unique identifier name and can propagate data
+ * RMI interface for a node A node own an unique identifier name and can
+ * propagate data
  * 
  * @author pierre falez, gaetan deflandre
  */
 public interface RMINode extends Remote {
-	
+
 	/**
-	 * this methods propagate data to its successors. If trace parameter is not null, he add itself into it before transmit it to its successors.
-	 * the uid parameter is used to recognize the message. this can be usefull is cycles are present in the network
-	 * @param data is an array of byte which contains the data to transfer
-	 * @param uid is an unique identifier for the message
-	 * @param trace is the node history in which the propagation pass through
-	 * @throws RemoteException 
+	 * this methods propagate data to its successors. If trace parameter is not
+	 * null, he add itself into it before transmit it to its successors. the uid
+	 * parameter is used to recognize the message. this can be useful is cycles
+	 * are present in the network
+	 * 
+	 * @param data
+	 *            is an array of byte which contains the data to transfer
+	 * @param uid
+	 *            is an unique identifier for the message
+	 * @param trace
+	 *            is the node history in which the propagation pass through
+	 * @throws RemoteException
 	 */
-	public void propagate(byte[] data, int uid, Trace trace) throws RemoteException;
+	public void propagate(byte[] data, int uid, Trace trace)
+			throws RemoteException;
 
 	/**
 	 * add a successor node to the current node
-	 * @param sucessor is a rmi node
-	 * @throws RemoteException 
+	 * 
+	 * @param sucessor
+	 *            is a rmi node
+	 * @throws RemoteException
 	 */
 	public void addSuccessor(RMINode sucessor) throws RemoteException;
-	
+
 	/**
-	 * @param name is an unique identifier as a String
-	 * @throws RemoteException 
+	 * @param name
+	 *            is an unique identifier as a String
+	 * @throws RemoteException
 	 */
 	public void setName(String name) throws RemoteException;
-	
+
 	/**
 	 * @return name of the node
 	 * @throws RemoteException
 	 */
 	public String getName() throws RemoteException;
-	
+
 	/**
 	 * Return the last received buffer with method propagate or null otherwise
+	 * 
 	 * @return data buffer
 	 * @throws RemoteException
 	 */
 	public byte[] getLastData() throws RemoteException;
-	
+
 	/**
 	 * Return the last received trace with method propagate or null otherwise
+	 * 
 	 * @return trace
 	 * @throws RemoteException
 	 */
